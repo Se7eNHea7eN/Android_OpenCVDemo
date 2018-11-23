cde04a81be504dabaf3f40fb8d69c055cf337918
zihao.liu <zihao.liu@asiainnovations.com>
2018-11-23 19:08:21 +0800

    Fd

--- app/src/main/java/com/se7en/opencvdemo/MainActivity.kt
+++ app/src/main/java/com/se7en/opencvdemo/MainActivity.kt
@@ -1,4 +1,4 @@
-package opencv.se7en.com.opencvdemo
+package com.se7en.opencvdemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
--- openCVLibrary343/src/main/java/org/opencv/android/CameraBridgeViewBase.java
+++ openCVLibrary343/src/main/java/org/opencv/android/CameraBridgeViewBase.java
@@ -1,12 +1,5 @@
 package org.opencv.android;
 
-import java.util.List;
-
-import org.opencv.BuildConfig;
-import org.opencv.R;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -19,6 +12,12 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import org.opencv.BuildConfig;
+import org.opencv.R;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+
+import java.util.List;
 
 /**
  * This is a basic class, implementing the interaction with Camera and OpenCV library.
@@ -46,7 +45,7 @@ public abstract class CameraBridgeViewBase extends SurfaceView implements Surfac
     protected int mMaxWidth;
     protected float mScale = 0;
     protected int mPreviewFormat = RGBA;
-    protected int mCameraIndex = CAMERA_ID_ANY;
+    protected int mCameraIndex = CAMERA_ID_FRONT;
     protected boolean mEnabled;
     protected FpsMeter mFpsMeter = null;
 
