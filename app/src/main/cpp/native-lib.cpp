349074c9cd4377173e5f7a646bce76feeac3cb21
zihao.liu <zihao.liu@asiainnovations.com>
2018-11-23 16:26:47 +0800

    init

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
+#include <android/log.h>
+#define  LOG_TAG    "JNI_PART"
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG, __VA_ARGS__)
+#define LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG, __VA_ARGS__)
+#define LOGF(...)  __android_log_print(ANDROID_LOG_FATAL,LOG_TAG, __VA_ARGS__)
+using namespace cv;
+using namespace std;
+extern "C"{
+
+}
---
+++ app/src/main/java/opencv/se7en/com/opencvdemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package opencv.se7en.com.opencvdemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+//        sample_text.text = stringFromJNI()
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("opencv_java3");
+            System.loadLibrary("opencv_java");
+            System.loadLibrary("native-lib")
+        }
+    }
+}
