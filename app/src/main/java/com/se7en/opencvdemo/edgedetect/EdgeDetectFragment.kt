54eea7eab261ee08e5822748fe904d841fd517d2
sherocyc <sherocyc@hotmail.com>
2018-12-10 00:24:23 +0800

    laplace

--- app/src/main/java/com/se7en/opencvdemo/edgedetect/EdgeDetectFragment.kt
+++ app/src/main/java/com/se7en/opencvdemo/edgedetect/EdgeDetectFragment.kt
@@ -1,54 +1,120 @@
 package com.se7en.opencvdemo.edgedetect
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import com.se7en.opencvdemo.BaseImageFragment
 import com.se7en.opencvdemo.ImageProcessor
+import com.se7en.opencvdemo.R
 import org.opencv.core.CvType
 import org.opencv.core.CvType.CV_8UC4
 import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
 
+
 class EdgeDetectFragment : BaseImageFragment() {
+    sealed class EdgeDetectStrategy {
+        abstract fun process(gray: Mat): Mat
 
-    private val sobel1 = Mat(3, 3, CvType.CV_32F).apply {
-        put(
-            0, 0,
-            floatArrayOf(
-                 1f, 2f, 1f,
-                 0f, 0f, 0f,
-                -1f,-2f,-1f
-            )
-        )
+        class Sobel(val width: Int, val height: Int) : EdgeDetectStrategy() {
+            private val sobel1 = Mat(3, 3, CvType.CV_32F).apply {
+                put(
+                    0, 0,
+                    floatArrayOf(
+                        1f, 2f, 1f,
+                        0f, 0f, 0f,
+                        -1f, -2f, -1f
+                    )
+                )
+            }
+
+            private val sobel2 = Mat(3, 3, CvType.CV_32F).apply {
+                put(
+                    0, 0,
+                    floatArrayOf(
+                        1f, 0f, -1f,
+                        2f, 0f, -2f,
+                        1f, 0f, -1f
+                    )
+                )
+            }
+            private val sobel1Mat: Mat
+            private val sobel2Mat: Mat
+
+            init {
+                sobel1Mat = Mat(width, height, CV_8UC4)
+                sobel2Mat = Mat(width, height, CV_8UC4)
+            }
+
+            override fun process(gray: Mat): Mat {
+                Imgproc.filter2D(gray, sobel1Mat, -1, sobel1)
+                Imgproc.filter2D(gray, sobel2Mat, -1, sobel2)
+                ImageProcessor.imageAdd(sobel1Mat, sobel2Mat, gray)
+                return gray
+            }
+        }
+
+        class Laplace(val width: Int, val height: Int) : EdgeDetectStrategy() {
+            private val laplace = Mat(3, 3, CvType.CV_32F).apply {
+                put(
+                    0, 0,
+                    floatArrayOf(
+                        1f, 1f, 1f,
+                        1f, -8f, 1f,
+                        1f, 1f, 1f
+                    )
+                )
+            }
+
+            override fun process(gray: Mat): Mat {
+                Imgproc.filter2D(gray, gray, -1, laplace)
+                return gray
+            }
+        }
     }
 
-    private val sobel2 = Mat(3, 3, CvType.CV_32F).apply {
-        put(
-            0, 0,
-            floatArrayOf(
-                1f,0f,-1f,
-                2f,0f,-2f,
-                1f,0f,-1f
-            )
-        )
-    }
+    lateinit var strategy: EdgeDetectStrategy
 
-    private lateinit var sobel1Mat: Mat
-    private lateinit var sobel2Mat: Mat
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sobel1Mat = Mat(width, height, CV_8UC4)
-        sobel2Mat = Mat(width, height, CV_8UC4)
+        strategy = EdgeDetectStrategy.Sobel(width, height)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.edgedetect, container, false).apply {
+            findViewById<Button>(R.id.strategySelect).apply {
+                setOnClickListener {
+                    val builder = AlertDialog.Builder(activity)
+                    val items = arrayOf(
+                        EdgeDetectStrategy.Sobel::class.simpleName,
+                        EdgeDetectStrategy.Laplace::class.simpleName
+                    )
+                    builder.setItems(
+                        items
+                    ) { _, which ->
+                        strategy = when (which) {
+                            0 -> EdgeDetectStrategy.Sobel(width, height)
+                            1 -> EdgeDetectStrategy.Laplace(width, height)
+                            else -> EdgeDetectStrategy.Sobel(width, height)
+                        }
+                        text = items[which]
+                    }
+                    builder.show()
+                }
+            }
+        }
     }
 
     override fun ProcessImage(rgba: Mat, gray: Mat): Mat {
-        Imgproc.filter2D(gray, sobel1Mat, -1, sobel1)
-        Imgproc.filter2D(gray, sobel2Mat, -1, sobel2)
-        ImageProcessor.imageAdd(sobel1Mat, sobel2Mat, gray)
-        return gray
+        return strategy.process(gray)
     }
 
-    override fun OriginalImage(rgba: Mat, gray: Mat): Mat {
-        ImageProcessor.imageInverse(ProcessImage(rgba, gray), gray)
-        return gray
-    }
+//    override fun OriginalImage(rgba: Mat, gray: Mat): Mat {
+//        ImageProcessor.imageInverse(ProcessImage(rgba, gray), gray)
+//        return gray
+//    }
 }
